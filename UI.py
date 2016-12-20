37d682c76a9ed544822c11b3e5d1b8d1188bf52b
zhen geng <zhen geng>
2016-12-20 01:12:28 -0800

    maya pymel scripts

---
+++ UI.py
@@ -0,0 +1,167 @@
+from PySide import QtCore, QtGui
+import pymel.core as pm
+import maya.OpenMaya as om
+import maya.OpenMayaUI as omui
+from shiboken import wrapInstance
+import vt_anim_export as vae
+import os
+
+
+def get_sel_shape():
+    shapeNode = None
+    try:
+        shapeNode = pm.selected()[0].getShape()
+    except:
+        # print "Please select one mesh"
+        shapeNode = None
+    return shapeNode
+
+
+def get_maya_window():
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window_ptr), QtGui.QWidget)
+
+
+@QtCore.Slot(object)
+def exportTexCallback(values):
+    # vae.createTex(*values)
+    def test(name, start, end, size, path):
+        print name
+        print start
+        print end
+        print size
+        print path
+    test(*values)
+
+
+elementList = [
+    {"name": "meshLable", "type": QtGui.QLabel,
+        "position": (0, 0, 1, 1), "arg": ["mesh"], "parent":"None"},
+    {"name": "objectLable", "type": QtGui.QLabel,
+        "position": (0, 1, 1, 3), "arg": [" "], "parent":"None"},
+    {"name": "setting", "type": QtGui.QGroupBox,
+     "position": (1, 0, 4, 4), "arg": ["Setting"], "parent":"None"},
+    {"name": "frameLable_1", "type": QtGui.QLabel,
+        "position": (2, 0, 1, 1), "arg": [" Start Frame"], "parent":"setting"},
+    {"name": "startFrame", "type": QtGui.QSpinBox,
+        "position": (2, 1, 1, 1), "arg": [], "parent":"setting"},
+    {"name": "frameLable_2", "type": QtGui.QLabel,
+        "position": (2, 2, 1, 1), "arg": [" End Frame"], "parent":"setting"},
+    {"name": "endFrame", "type": QtGui.QSpinBox,
+        "position": (2, 3, 1, 1), "arg": [], "parent":"setting"},
+    {"name": "indexTexSizeLable", "type": QtGui.QLabel,
+     "position": (3, 0, 1, 2), "arg": [" indexTexSize"], "parent":"setting"},
+    {"name": "indexTexSize", "type": QtGui.QComboBox,
+        "position": (3, 2, 1, 2), "arg": [], "parent":"setting"},
+    {"name": "fileDirLabel", "type": QtGui.QLabel,
+     "position": (4, 0, 1, 1), "arg": ["  Save As"], "parent":"setting"},
+    {"name": "fileDirButton", "type": QtGui.QPushButton,
+        "position": (4, 1, 1, 1), "arg": [''], "parent":"setting"},
+    {"name": "fileDir", "type": QtGui.QLineEdit,
+     "position": (4, 2, 1, 2), "arg": [], "parent":"setting"},
+    {"name": "info", "type": QtGui.QLabel,
+     "position": (5, 0, 1, 2), "arg": ["......"], "parent":"None"},
+    {"name": "exportButton", "type": QtGui.QPushButton,
+     "position": (5, 2, 1, 2), "arg": ["Export"], "parent":"None"}
+]
+
+
+class UILayout(QtGui.QMainWindow):
+    export = QtCore.Signal(object)
+
+    def setMesh(self, *args, **kwargs):
+        self.dagPath = get_sel_shape()
+        labelText = " "
+        if not(self.dagPath is None):
+            labelText = self.dagPath.nodeName()
+        self.elements["objectLable"].setText(labelText)
+
+    def __init__(self, parent=get_maya_window()):
+        super(UILayout, self).__init__(parent)
+        self.setWindowTitle('VertexAnimationExporter')
+        self.setFixedSize(350, 200)
+        self.elements = dict()
+        self.container = QtGui.QWidget(self)
+        self.exportedData = ["dagPath", 0, 10, 1, "filePath"]
+        self.idx = om.MEventMessage.addEventCallback(
+            "SelectionChanged", self.setMesh)
+        self.dagPath = None
+
+    def createLayout(self, elementList):
+
+        grid = QtGui.QGridLayout()
+        for element in elementList:
+            name = element["name"]
+            pos = element["position"]
+            arg = element["arg"]
+            widget_parent_name = element["parent"]
+            widget_parent = self.container  # default
+            if (widget_parent_name != "None"):
+                widget_parent = self.elements[widget_parent_name]
+            widget = element["type"](*arg)
+            widget.setParent(widget_parent)
+            self.elements[name] = widget
+            grid.addWidget(widget, pos[0], pos[1], pos[
+                           2], pos[3])
+
+        self.container.setLayout(grid)
+        self.setCentralWidget(self.container)
+
+    def exportOnClick(self):  # define the function to send out message
+        self.updateExportedData()
+        self.export.emit(self.exportedData)
+
+    def dirOnClick(self):
+        path, _ = QtGui.QFileDialog.getSaveFileName(
+            self, "Save File", os.getcwd())
+        self.elements["fileDir"].setText(path)
+        return path
+
+    def updateExportedData(self):
+        self.exportedData[0] = self.elements["objectLable"].text()
+        self.exportedData[1] = self.elements["startFrame"].value()
+        self.exportedData[2] = self.elements["endFrame"].value()
+        self.exportedData[3] = self.elements["indexTexSize"].currentIndex()
+        self.exportedData[4] = self.elements["fileDir"].text()
+
+    def closeEvent(self, event):
+        om.MMessage.removeCallback(self.idx)
+        event.accept()  # let the window close
+
+
+def setupElement(uiWindow):
+    uiWindow.elements["startFrame"].setMaximum(300)
+    uiWindow.elements["endFrame"].setMaximum(300)
+    fileButton = uiWindow.elements["fileDirButton"]
+    fileButton.setIcon(
+        fileButton.style().standardIcon(QtGui.QStyle.SP_DirIcon))
+    indexTexSize = uiWindow.elements["indexTexSize"]
+    indexTexSize.addItems(["32*32", "64*64", "128*128",
+                           "256*256", "512*512", "1024*1024"])
+    indexTexSize.setCurrentIndex(1)
+
+
+def setupEvent(uiWindow):
+    # inside function connection
+    exportButton = uiWindow.elements["exportButton"]
+    exportButton.clicked.connect(uiWindow.exportOnClick)
+
+    fileButton = uiWindow.elements["fileDirButton"]
+    fileButton.clicked.connect(uiWindow.dirOnClick)
+    # outside function connection
+    uiWindow.export.connect(exportTexCallback)
+
+
+def main():
+    # app = QtGui.QApplication([])
+    # assign a callback function
+
+    win = UILayout(get_maya_window())
+    win.createLayout(elementList)
+    setupElement(win)
+    setupEvent(win)
+
+    win.show()
+    print 'executing'
+    # win.exec_()
+    return
---
+++ vt_anim_export_withpyside.py
@@ -0,0 +1,371 @@
+# covert vertex animation into shader readable maps for Unity
+# export the vertex motion into maps
+# this script will not change the state of maya scene
+
+import struct
+import pymel.core as pm
+from maya import OpenMaya
+import math
+from PySide import QtGui
+QImage = QtGui.QImage
+
+shapeNode = None
+Epsilon = 1e-6
+
+
+# get selection shape
+def get_sel_shape():
+    try:
+        shapeNode = pm.selected()[0].getShape()
+    except AttributeError:
+        print "Please select one mesh"
+        shapeNode = None
+    return shapeNode
+
+
+def get_vtList(shapeNode):
+    # the simple one just use pymel
+    # will change into api function
+    pointList = shapeNode.getPoints()
+    return pointList
+
+
+def get_UVList(shapeNode):
+    # the simple one just use pymel
+    # will change into api function for speed
+    UVList = shapeNode.getUVs()
+    return UVList
+
+
+def vt_uv_map(shapeNode):
+    # vertex id as key and UVs id list as value
+    # will change into api function
+    vt_UV = {}
+    vertexs = shapeNode.vtx
+
+    for vt_id, vertex in enumerate(vertexs):
+        # check if already create key for the vertex id
+        Us, Vs, FaceIds = vertex.getUVs()
+        UVs = zip(Us, Vs)
+        vt_UV[vt_id] = list(set(UVs))  # remove duplicate
+    return vt_UV
+
+
+def get_NormalList(shapeNode):
+    return [vtx.getNormal() for vtx in shapeNode.vtx]
+
+
+def get_vertex_tangent(vt, vtId, shapeNode, normal):
+    total = pm.datatypes.Vector()
+    for face in vt.connectedFaces():
+        total += shapeNode.getFaceVertexTangent(face.currentItemIndex(), vtId)
+    total.normalize()
+    # Orth with Gram_Schmidt method
+    tangent = total - normal * total.dot(normal)
+    return tangent
+
+
+def get_vertex_tangent_ids(shapeNode):
+    result = []
+    for vt_id, vertex in enumerate(shapeNode.vtx):
+        ids = []
+        for face in vertex.connectedFaces():
+            ids.append(shapeNode.getTangentId(face.currentItemIndex(), vt_id))
+        result.append(ids)
+    return result
+
+
+def get_per_vertex_tangent(tangentList, tangent_ids, vt_id, normal):
+    total = pm.datatypes.Vector()
+    for tangent_id in tangent_ids[vt_id]:
+        total += tangentList[tangent_id]
+    total.normalize()
+    # Orth with Gram_Schmidt method
+    tangent = total - normal * total.dot(normal)
+    return tangent
+
+
+def get_TangentList(shapeNode):
+    tangentList = []
+    for vt_id, vertex in enumerate(shapeNode.vtx):
+        normal = vertex.getNormal()
+        tangent = get_vertex_tangent(vertex, vt_id, shapeNode, normal)
+        tangentList.append(tangent)
+    return tangentList
+
+
+def vt_info_list(shapeNode, tangent_ids, tangentList):
+    vt_infos = []
+    vertexs = shapeNode.vtx
+    # iterate throgh all the vertex
+    for vt_id, vertex in enumerate(vertexs):
+        position = vertex.getPosition(space="object")
+
+        Us, Vs, FaceIds = vertex.getUVs()
+        UVs = zip(Us, Vs)
+        # remove duplicate uvs
+        UVlist = list(set(UVs))
+
+        normal = vertex.getNormal(space="object")  # one or more?
+        tangent = get_per_vertex_tangent(tangentList, tangent_ids,
+                                         vt_id, normal)
+        # build a map
+        info = {"Position": position, "UV": UVlist,
+                "Normal": normal, "Tangent": tangent}
+        vt_infos.append(info)
+    return vt_infos
+
+
+def get_dis_buffer(startFrame, endFrame, shapeNode):
+
+    time = endFrame - startFrame + 1
+    # get the current frame
+    oldFrame = pm.getCurrentTime()
+    # set the current time to the start time
+    # prepare tangent data
+    tangentList = shapeNode.getTangents(space="object")
+    tangent_ids = get_vertex_tangent_ids(shapeNode)
+    # get the info data
+    vt_infoList = vt_info_list(shapeNode, tangent_ids, tangentList)
+    pm.setCurrentTime(startFrame)
+    # create a vt_id list to record the moved vertex id
+    moved_vt_id = set()
+    # creat wrap list to store data and initalize it
+    dis_buffer = []
+
+    def dis_list(frame):
+        # get current frame vertex position
+        result = []
+
+        pm.setCurrentTime(frame)
+        vt_infoList_cur = vt_info_list(shapeNode, tangent_ids, tangentList)
+
+        for vt_id, vt_info in enumerate(vt_infoList):
+            pos_trans = vectorDiff(vt_infoList[vt_id]["Position"],
+                                   vt_infoList_cur[vt_id]["Position"])
+            pos_color = convert_vector_to_argb(pos_trans)
+
+            normal_trans = vectorDiff(vt_infoList[vt_id]["Normal"],
+                                      vt_infoList_cur[vt_id]["Normal"])
+            normal_color = convert_vector_to_argb(normal_trans)
+
+            tangent_trans = vectorDiff(vt_infoList[vt_id]["Tangent"],
+                                       vt_infoList_cur[vt_id]["Tangent"])
+            tangent_color = convert_vector_to_argb(tangent_trans)
+
+            dis_info = {"Position": pos_color,
+                        "Normal": normal_color,
+                        "Tangent": tangent_color}
+            result.append(dis_info)
+
+            if ((not vectorEqualZero(pos_trans)) or
+               (not vectorEqualZero(normal_trans)) or
+               (not vectorEqualZero(tangent_trans))):
+                moved_vt_id.add(vt_id)  # modify the outside list
+
+        return result
+
+    for shiftFrame in range(time):
+        currentFrame = startFrame + shiftFrame
+        dis_buffer.append(dis_list(currentFrame))
+
+    # setback the old time
+    pm.setCurrentTime(oldFrame)
+    moved_vt_id_list = list(moved_vt_id)
+
+    return dis_buffer, moved_vt_id_list
+
+
+# post buffer clean up
+
+def buffer_resort(data):
+    return zip(*data)  # remap data
+
+
+def vectorDiff(v1, v2):
+    size = len(v1)
+    try:
+        result = [v2[i] - v1[i] for i in range(size)]
+        return result
+    except:
+        print 'cannot do vecter diff'
+        return None
+
+
+def vectorEqualZero(v):
+    total = 0
+    for value in v:
+        total += value * value
+    return total <= Epsilon
+
+
+def convert_vector_to_argb(v):
+    scale = 1
+
+    scale = int(math.ceil(math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)))
+    if scale > 255:
+        print 'unable to convert displacement vector into color'
+
+    if scale == 0:
+        return struct.pack("I", 0)
+
+    R = int(round(127.5 + 127.5 * (v[0] / scale)))  # 127.5 is half 255
+    G = int(round(127.5 + 127.5 * (v[1] / scale)))
+    B = int(round(127.5 + 127.5 * (v[2] / scale)))
+    A = scale
+    pixel = A * 256 * 256 * 256 + R * 256 * 256 + G * 256 + B
+    return struct.pack("I", pixel)
+
+
+def convert_int_to_argb(num):
+    # alpha channel is 0
+    return struct.pack('I', num)
+
+
+def convert_two_int_argb(num1, num2):
+    # AR--num1 GB--num2
+    return struct.pack('I', num1 * 256 * 256 + num2)
+
+
+def imageWriteInt(imgData_ptr, size, u, v, value):
+    x = int(size * u) % size
+    y = int(size * v) % size
+    index = 4 * (y * size + x)
+
+    try:
+        if not imgData_ptr[index: index + 4] == chr(0) * 4:
+            raise RuntimeError(
+                'Vertex map to same pixel,'
+                ' try to enlarge the image size or readjust the UV')
+        imgData_ptr[index: index + 4] = convert_int_to_argb(value)
+        # not modify the imgData pointer
+    except:
+        print x, y
+
+    return
+
+
+def data_image_size(frameNum, vtNum):
+    # vtNum is the moved vt number
+    pixelNum = 3 * frameNum * (vtNum + 1) + 1
+    # 3 pixel per vertex per frame
+    # two extra pixels used for
+    # 1  first line is to store the zero value
+    # 2 last pixel is to store the frameNum,vtNum information
+    size = int(math.pow(2, math.ceil(math.log(pixelNum, 4))))
+    # get the dimention of the image
+    return size
+
+
+def create_data_img(size):
+    img = QImage(size, size, QImage.Format_ARGB32)
+    img.fill(0)
+    return img
+
+
+def create_index_img(size=32):
+    img = QImage(size, size, QImage.Format_ARGB32)
+    # fill the image with empty color
+    img.fill(0)
+    return img
+
+
+def set_index_img(img, moved_vtList, vt_uv):
+    # get the index in the moved_vt
+    # image size
+    size = img.width()  # assume width equals height
+    imgData_ptr = img.bits()  # load img data
+    # debug to print out all the buffer data
+
+    for vtId in vt_uv:
+        if vtId not in moved_vtList:
+            # write to the empty cell
+            value = 0
+            for UVs in vt_uv[vtId]:  # this can wrap into imageWrite Function
+                u, v = UVs
+                imageWriteInt(imgData_ptr, size, u, v, value)
+        else:
+            value = moved_vtList.index(vtId) + 1
+            value = 255 * 256 * 256 * 256
+            for UVs in vt_uv[vtId]:
+                u, v = UVs
+                imageWriteInt(imgData_ptr, size, u, v, value)
+
+    return  # the img should be modified
+
+
+def set_data_img(img, frameNum, vtNum, bufferData, vtIds):
+    imgData_ptr = img.bits()  # load image data
+    buffer_size = img.byteCount()
+    # creat an empty framNum elements
+    currentPosition = 0  # current data list positon
+
+    for i in range(frameNum * 3):
+        # get x and y position
+        imgData_ptr[i * 4: i * 4 + 4] = struct.pack("I", 0)
+
+    currentPosition = frameNum * 3
+
+    for vtId in vtIds:
+        for i in range(frameNum):
+            # get x and y position
+            # retrive the data
+            # print len(bufferData)
+            data = bufferData[vtId][i]
+            # put data into image
+            imgData_ptr[currentPosition * 4:
+                        currentPosition * 4 + 4] = data["Position"]
+            imgData_ptr[currentPosition * 4 + 4:
+                        currentPosition * 4 + 8] = data["Normal"]
+            imgData_ptr[currentPosition * 4 + 8:
+                        currentPosition * 4 + 12] = data["Tangent"]
+            currentPosition += 3
+
+    # And last pixel, write the frameNum and vtId size data
+    vt_size = len(vtIds)
+    # convert 2 int into RGBA
+    info_color = convert_two_int_argb(frameNum, vt_size)
+    # write the info color in to last pixel
+    imgData_ptr[buffer_size - 4: buffer_size] = info_color
+
+    return
+
+
+def indexImageTest(startFrame, endFrame):
+    shapeNode = get_sel_shape()
+    vt_uv = vt_uv_map(shapeNode)  # result is UV lists map
+    # print vt_uv
+    data, vtIds = get_dis_buffer(startFrame, endFrame, shapeNode)
+    print type(vtIds)
+    # print data
+    # UList, VList = get_UVList(shapeNode)
+    # del_index = [i for i in range(size) not in vtIds]
+    # print vtIds
+    img = create_index_img(64)
+    set_index_img(img, vtIds, vt_uv)
+
+    img.save('C:/mypy/indexImageTest.png', 'PNG')
+    return
+
+
+def dataImageTest(startFrame, endFrame):
+    shapeNode = get_sel_shape()
+    data, vtIds = get_dis_buffer(startFrame, endFrame, shapeNode)
+    data = buffer_resort(data)
+    vtId_size = len(vtIds)
+    print vtId_size
+    frameNum = endFrame - startFrame + 1
+    img_size = data_image_size(frameNum, vtId_size)
+    img = create_data_img(img_size)
+    set_data_img(img, frameNum, vtId_size, data, vtIds)
+
+    img.save('C:/mypy/dataImageTest.png', 'PNG')
+    return
+
+
+def dataTest(startFrame, endFrame):
+    shapeNode = get_sel_shape()
+    data, vtIds = get_dis_buffer(startFrame, endFrame, shapeNode)
+    new_data = buffer_resort(data)
+    new_data = [j for i, j in enumerate(new_data) if i in vtIds]
+    return len(new_data)
